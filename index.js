fae9a15536903f208e656442e584767974546909
Ori Newman <orinewman1@gmail.com>
2016-07-25 01:07:26 +0300

    remove requirement for lodash.noop

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * Created by ori on 16/07/16.
  */
 var Promise = require('bluebird');
-var noop = require('lodash.noop');
 module.exports = function shouldReject(promise) {
     return promise.then(function () {
         return Promise.reject();
